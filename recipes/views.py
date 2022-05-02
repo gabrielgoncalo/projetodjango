51a3450669653e1e42d418d3f12dd1dc5a64ac20
gabrielgoncalo <gabriel.tchutcha@hotmail.com.br>
2022-05-01 22:53:05 -0300

    add detail, list e update #1

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,11 @@
+from multiprocessing import context
 from django.shortcuts import render
-
+from django.urls import path
 from .forms import recipesForm
 from .models import recipesModel
-
-# test
+from django.shortcuts import (get_object_or_404,
+                              render,
+                              HttpResponseRedirect)
 
 
 def create_view(request):
@@ -15,3 +17,33 @@ def create_view(request):
 
     context['form'] = form
     return render(request, "create_view.html", context)
+
+
+def list_view(request):
+    context = {}
+
+
+    context["dataset"] = recipesModel.objects.all()
+    return render(request,"list_view.html", context)
+
+
+def detail_view(request, id):
+    context = {}
+
+    context["data"] = recipesModel.objects.get(id = id)
+    return render(request, "detail_view.html", context)
+
+def update_view(request, id):
+    context = {}
+
+    obj = get_object_or_404(recipesModel, id = id)
+    form = recipesForm(request.POST or None, instance = obj)
+
+    if form.is_valid():
+        form.save()
+        return HttpResponseRedirect("/"+id)
+
+    context["form"] = form
+    return render(request, "update_view.html", context)
+
+
